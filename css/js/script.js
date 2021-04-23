d7e16d315a4f8beb13db79a6d8a9280f3c44e9e8
smithbh1 <smithbh129@gmail.com>
2021-04-23 10:33:43 -0600

    completed site

--- css/js/script.js
+++ css/js/script.js
@@ -1,15 +1,3 @@
-let daynames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-let month = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"]
+document.querySelector("#lastUpdate").textContent = document.lastModified ;
 
-let d= new Date(document.lastModified) ;
-let dayName = daynames[d.getDay()] ;
-let monthName = month[d.getMonth()] ;
-
-let fulldate = dayName + ", " + monthName + " " + d.getDate() + " " + d.getFullYear() ;
-
-let current_year = d.getFullYear() ;
-
-document.getElementById("currentYear").textContent = current_year ;
-document.getElementById("lastUpdate").textContent = fulldate ;
-
-
+document.querySelector("#currentYear").textContent = new Date().getFullYear() ;
