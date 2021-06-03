600091e8e3072ff0b0d01177026ffe0e6a107082
smithbh1 <smithbh129@gmail.com>
2021-06-03 07:18:57 -0600

    script fix

--- lesson7/js/lazyload_script.js
+++ lesson7/js/lazyload_script.js
@@ -7,6 +7,10 @@ const loadImages = (image) => {
   };
 };
 
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 40px 0px"
+}
 if('IntersectionObserver' in window) {
 
     const observer = new IntersectionObserver((items, observer) => {
@@ -16,12 +20,12 @@ if('IntersectionObserver' in window) {
           observer.unobserve(item.target);
         }
       });
-    });
+    }, imgOptions);
 
     imagesToLoad.forEach((img) => {
       observer.observe(img);
     });
-    
+
   } else {
     imagesToLoad.forEach((img) => {
       loadImages(img);
