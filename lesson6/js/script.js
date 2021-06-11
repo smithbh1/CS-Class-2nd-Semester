a56ff96de7d4da18ac5eb4f74decf80b089d38b2
smithbh1 <smithbh129@gmail.com>
2021-06-11 08:16:40 -0600

    js fix

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -78,23 +78,3 @@ function number_of_days(start, end){
 
 	document.querySelector("#last_visit").innerHTML = `Your last visit was ${day_difference} days ago.`;
 };
-//New "Thank You" window
-function thank_you(){
-	valid = false;
-	fname = document.querySelector("#fname");
-	phone = document.querySelector("#phone");
-	zipcode = document.querySelector("#zipcode");
-	submit = document.querySelector("#submit");
-
-
-	if (fname.length >= 5 && fname.length != null && phone.length == 10 && phone.length != null && zipcode.length == 5 && zipcode.length != null){
-		valid = true;
-		window.open("thank-you.html","Ratting","width=550,height=550,left=150,top=200,toolbar=0,status=0,");
-	}
-
-	if (valid == true){
-		window.open("thank-you.html","Ratting","width=550,height=550,left=150,top=200,toolbar=0,status=0,");
-	}
-	
-	
-}
